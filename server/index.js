d8dd02c3354a8441ab2bf7536eeb5b912fc90ed5
yolixtly <yolixtly_13@hotmail.com>
2016-09-07 19:30:02 -0400

    STEP 1: Print something with Backend

---
+++ server/index.js
@@ -0,0 +1,39 @@
+import 'babel-polyfill';
+import express from 'express';
+
+const HOST = process.env.HOST;
+const PORT = process.env.PORT || 8080;
+
+console.log(`Server running in ${process.env.NODE_ENV} mode`);
+
+let app = express();
+
+app.use(express.static(process.env.CLIENT_PATH));
+
+
+app.get('/api/hello', function(request, response) {
+    response.json({
+        message: 'STEP 1: Hello From Express',
+        description: 'Run the command npm start and visit localhost:PORT',
+        nextStep: 'Go to the frontend and render something with a component'    
+    });
+});
+
+
+function runServer() {
+    return new Promise((resolve, reject) => {
+        app.listen(PORT, HOST, (err) => {
+            if (err) {
+                console.error(err);
+                reject(err);
+            }
+
+            const host = HOST || 'localhost';
+            console.log(`Listening on ${host}:${PORT}`);
+        });
+    });
+}
+
+if (require.main === module) {
+    runServer();
+}
